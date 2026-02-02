4cf516f5b0f61c7f1b764238df6afafe9c0bf6a3
Riven91 <Elementum_Publishing@proton.me>
2026-02-02 02:04:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,12 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000"]
-    }
-  }
+      allowedOrigins: ["localhost:3000"],
+    },
+  },
+
+  // WICHTIG: damit wir echte Client-Fehler sehen
+  productionBrowserSourceMaps: true,
 };
 
 export default nextConfig;
